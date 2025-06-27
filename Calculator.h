84f7b4a6ebb870cd8e6ba3b821288e0e3e546292
Julux-yare <juluxyare@gmail.com>
2025-06-27 21:51:50 +0200

    Create Calculator.h

---
+++ Calculator.h
@@ -0,0 +1,13 @@
+#ifndef CALCULATOR_H
+#define CALCULATOR_H
+
+class Calculator {
+public:
+    // Metoder för de olika räknesätten
+    double add(double a, double b);      // Addition
+    double subtract(double a, double b); // Subtraktion
+    double multiply(double a, double b); // Multiplikation
+    double divide(double a, double b);   // Division
+};
+
+#endif
